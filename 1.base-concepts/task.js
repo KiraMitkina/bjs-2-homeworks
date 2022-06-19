68bd012d5fdd4f344402335101f022802d991734
KiraMitkina <91010655+KiraMitkina@users.noreply.github.com>
2022-06-19 16:53:47 +0300

    изменен

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,26 @@
-function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
-}
+"use strict";
+
+
+
+  function solveEquation(a, b, c) {
+    let arr;
+    let root1;
+    let root2;
+    let discriminant = b ** 2 - 4 * a * c;
+    if (discriminant < 0){
+      arr = [];
+    }
+    else if (discriminant == 0){
+      root1 = - b /( 2 * a );
+      arr = [root1];
+    } else {
+      root1 = (-b + Math.sqrt(discriminant) )/(2*a);
+      root2 = (-b - Math.sqrt(discriminant) )/(2*a);
+      arr = [ root1, root2];
+    }
+    
+    return arr; // array
+  }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
