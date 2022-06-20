22c509c30276709da69788104e836a32b3886622
KiraMitkina <91010655+KiraMitkina@users.noreply.github.com>
2022-06-20 10:34:04 +0300

    Add files via upload
    update

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,60 @@
+'use strict'
+
+
+
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
-
-  // Ваш код
-
-  return { min: min, max: max, avg: avg };
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  let avg = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+  avg = parseFloat((sum / arr.length).toFixed(2));
+  return { min:min, max:max, avg:avg };
 }
 
 // Задание 2
-function worker(arr) {
-  let sum;
+const worker = function (arr) {
+  let sum = 0;
 
-  // Ваш код
+    for (let i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+    return sum;
+  }
 
-  return sum;
-}
 
-function makeWork(arrOfArr, func) {
-  let max;
+  function makeWork(arrOfArr, func) {    
+  let max = -Infinity;
 
-  // Ваш кода
-  // for ...
-  
-  return max;
-}
+    for (let i = 0; i < arrOfArr.length; i++) {
+      if (func(arrOfArr[i]) > max) {
+        max = func(arrOfArr[i])
+      }
+    }
+    return max;
+  }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
-}
+  let min = Infinity;
+  let max = -Infinity;
+
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > max) {
+        max = arr[i];
+    }
+      if (arr[i] < min) {
+        min = arr[i];
+      }
+  }
+  return Math.abs(max - min);
+  }
