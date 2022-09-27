afc0f6eda61a01c9c0e8e97a6a55e0367a3c2c33
KiraMitkina <91010655+KiraMitkina@users.noreply.github.com>
2022-09-27 17:58:51 +0300

    Add files via upload
    Upload

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,20 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
-  return result; // boolean
+  if (arr1.length !== arr2.length) {
+    result = false
+  } else {
+    result = arr1.every((element, i) => element === arr2[i])
+  }
+  return result;
 }
 
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr.filter((item) => item > 0)
+    .filter((item) => item % 3 === 0)
+    .map((item) => item * 10);
 
-  return resultArr; // array
+  return resultArr;
 }
+
