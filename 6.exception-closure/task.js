52b5dd7a91e9c656b8f04d9e9a458a69237c0072
KiraMitkina <91010655+KiraMitkina@users.noreply.github.com>
2022-09-29 19:22:26 +0300

    Add files via upload
    homework

---
+++ 6.exception-closure/task.js
@@ -0,0 +1,47 @@
+function parseCount(value) {
+    let result = Number.parseInt(value);
+    if (isNaN(result)) {
+        throw new Error("Невалидное значение");
+    }
+    return result;
+}
+
+function validateCount(value) {
+    try {
+        return parseCount(value);
+    } catch (err) {
+        return err;
+    }
+}
+
+
+class Triangle {
+    constructor(a, b, c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+
+        if (a + b < c || a + c < b || c + b < a) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+    }
+    getPerimeter() {
+        return this.a + this.b + this.c;
+    }
+    getArea() {
+        const p = this.getPerimeter() / 2;
+        const area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+        return Number(area.toFixed(3));
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+    } catch {
+        return {
+            getPerimeter: () => "Ошибка! Треугольник не существует",
+            getArea: () => "Ошибка! Треугольник не существует"
+        }
+    }
+}
